7335ecc23eb23671b778091f3d9dea44157bfcd8
BankeMihai <143846835+BankeMihai@users.noreply.github.com>
2023-09-05 02:25:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -39,38 +39,26 @@ function speakText() {
     utterance.voice = window.speechSynthesis.getVoices()[selectedVoiceIndex];
     speechSynthesis.speak(utterance);
 }
-// Function to generate and download audio
-// Function to generate and download audio
-function downloadAudio() {
-    const text = document.getElementById("text").value;
+
+// Event listener for the download button
+downloadButton.addEventListener("click", async () => {
+    const text = textInput.value;
     const pitch = parseFloat(document.getElementById("pitch").value);
     const rate = parseFloat(document.getElementById("rate").value);
-    const voiceSelect = document.getElementById("voice");
-    const selectedVoiceIndex = voiceSelect.options[voiceSelect.selectedIndex].value;
 
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.pitch = pitch;
-    utterance.rate = rate;
-    utterance.voice = speechSynthesis.getVoices()[selectedVoiceIndex];
+    try {
+        const response = await fetch("/tts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ text, pitch, rate }),
+        });
 
-    // Prevent the speech from playing
-    utterance.volume = 0;
+        if (response.ok) {
+            const blob = await response.blob();
 
-    // Synthesize the speech
-    speechSynthesis.speak(utterance);
-
-    // Create a new AudioContext to access the audio data
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-    // Wait for the audio to be ready
-    utterance.onend = async function () {
-        const audioBuffer = audioContext.createBufferSource();
-        audioBuffer.buffer = audioContext.createBuffer(1, utterance.audioBuffer.length, audioContext.sampleRate);
-        audioBuffer.buffer.getChannelData(0).set(utterance.audioBuffer);
-
-        // Create a Blob from the audioBuffer
-        try {
-            const blob = await audioBufferToBlob(audioBuffer.buffer);
+            // Create a URL for the Blob
             const audioUrl = URL.createObjectURL(blob);
 
             // Set the download link's attributes and trigger the download
@@ -78,36 +66,14 @@ function downloadAudio() {
             downloadLink.href = audioUrl;
             downloadLink.download = "speech.mp3";
             downloadLink.style.display = "block"; // Show the link
-        } catch (error) {
-            console.error("Error creating Blob:", error);
+        } else {
+            console.error("Failed to generate audio:", response.status, response.statusText);
         }
-    };
-}
+    } catch (error) {
+        console.error("Error generating audio:", error);
+    }
+});
 
-// Function to convert an AudioBuffer to a Blob
-function audioBufferToBlob(buffer) {
-    return new Promise((resolve, reject) => {
-        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        const destination = audioContext.createMediaStreamDestination();
-        const recorder = new MediaRecorder(destination.stream);
-
-        recorder.ondataavailable = (e) => {
-            resolve(e.data);
-            audioContext.close();
-        };
-
-        recorder.onstop = () => {
-            audioContext.close();
-        };
-
-        const source = audioContext.createBufferSource();
-        source.buffer = buffer;
-        source.connect(destination);
-        source.start();
-        recorder.start();
-        recorder.stop();
-    });
-}
 
 speakButton.addEventListener("click", speakText);
-downloadButton.addEventListener("click", downloadAudio);
+
