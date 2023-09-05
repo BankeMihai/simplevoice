22a70077b610886bcc562f8f4d3c50844cfb08af
BankeMihai <143846835+BankeMihai@users.noreply.github.com>
2023-09-05 19:30:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,29 @@
-// script.js
-const textInput = document.getElementById("text");
-const speakButton = document.getElementById("speak");
-const downloadButton = document.getElementById("download");
-const pitchInput = document.getElementById("pitch");
-const rateInput = document.getElementById("rate");
-const audioElement = document.getElementById("audio");
+const textToSpeechInput = document.getElementById('text-to-speech-input');
+const speakButton = document.getElementById('speak-button');
+const audioPlayer = document.getElementById('audio-player');
 
-// Define a list of available voices
-const availableVoices = [
-    { name: "Voice 1", lang: "en-US" },
-    { name: "Voice 2", lang: "en-GB" },
-    { name: "Voice 3", lang: "es-ES" }
-];
-
-// Function to populate the voice selection dropdown
-function populateVoiceDropdown() {
-    const voiceSelect = document.getElementById("voice");
-
-    availableVoices.forEach((voice, index) => {
-        const option = document.createElement("option");
-        option.value = index;
-        option.text = voice.name;
-        voiceSelect.appendChild(option);
-    });
-}
-
-// Function to generate and play audio
-function generateAudio() {
-    const text = document.getElementById('text').value;
-    const pitch = parseFloat(document.getElementById('pitch').value);
-    const rate = parseFloat(document.getElementById('rate').value);
-    const voiceSelect = document.getElementById('voice');
-    const selectedVoiceIndex = voiceSelect.options[voiceSelect.selectedIndex].value;
-
-    const requestOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text, pitch, rate }),
-    };
-
-    return fetch('/tts', requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-            const audioUrl = data.audioUrl;
-            const audio = document.getElementById('audio');
-            audio.src = audioUrl;
-            audio.style.display = 'block'; // Show the audio player
-            audio.play(); // Play the generated audio
+speakButton.addEventListener('click', () => {
+    const text = textToSpeechInput.value.trim();
+    if (text !== '') {
+        // Send the text to the server to generate an audio file
+        fetch('/synthesize', {
+            method: 'POST',
+            body: JSON.stringify({ text }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
         })
-        .catch((error) => {
+        .then(response => response.blob())
+        .then(blob => {
+            // Create an object URL for the audio blob
+            const audioURL = URL.createObjectURL(blob);
+            
+            // Set the audio player source and play it
+            audioPlayer.src = audioURL;
+            audioPlayer.play();
+        })
+        .catch(error => {
             console.error('Error:', error);
         });
-}
-
-// Event listener for the speak button
-document.getElementById('speak').addEventListener('click', () => {
-    // Generate the audio and play it
-    generateAudio();
+    }
 });
-
